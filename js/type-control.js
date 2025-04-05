506ceefde4821eacc767df949de3ce2a76298213
QuangLy98 <102861589+QuangLy98@users.noreply.github.com>
2025-04-05 17:32:07 +0700

    Add files via upload

---
+++ js/type-control.js
@@ -0,0 +1,7 @@
+// JavaScript Document
+	var typed = new Typed('.element', {
+               	strings: ["Front-end Developer", "UX/UI Designer","Recruiter"],
+       	 	typeSpeed:50,
+        		backSpeed:50,
+        		loop:true,
+        		});
